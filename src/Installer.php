2089b68df3f78e9bd36ad13ae9c0b2707be9e91c
wkania <wojciech.kania+github@gmail.com>
2023-09-22 14:15:30 +0200

    [Fix] Installer didn't create table users_datahub_config

--- src/Installer.php
+++ src/Installer.php
@@ -28,36 +28,45 @@ class Installer extends SettingsStoreAwareInstaller
 
         if (method_exists($db, 'getSchemaManager')) {
             $schema = $db->getSchemaManager()->createSchema();
+            $currentSchema = $db->getSchemaManager()->createSchema();
         } else {
             $schema = $db->createSchemaManager()->introspectSchema();
+            $currentSchema = $db->createSchemaManager()->introspectSchema();
         }
 
 
         // create table
-        if ($schema->hasTable(self::CONFIG_TABLE)) {
-            $table = $schema->getTable(self::CONFIG_TABLE);
-            $table->addColumn('id', 'int', ['length' => 11, 'autoincrement' => true, 'notnull' => true]);
+        if (!$schema->hasTable(self::CONFIG_TABLE)) {
+            $table = $schema->createTable(self::CONFIG_TABLE);
+            $table->addColumn('id', 'integer', ['length' => 11, 'autoincrement' => true, 'notnull' => true]);
             $table->addColumn('data', 'text', ['notnull' => false]);
-            $table->addColumn('userId', 'int', ['length' => 11, 'notnull' => true]);
+            $table->addColumn('userId', 'integer', ['length' => 11, 'notnull' => true]);
             $table->setPrimaryKey(['id']);
             $table->addForeignKeyConstraint(
                 'users',
                 ['userId'],
                 ['id'],
-                ['onDelete' => 'CASCADE']
+                ['onDelete' => 'CASCADE'],
+                'fk_datahub_users'
             );
         }
         // create table
-        if ($schema->hasTable(self::UPLOAD_SESSION_TABLE)) {
-            $table = $schema->getTable(self::UPLOAD_SESSION_TABLE);
-            $table->addColumn('id', 'varchar', ['length' => 255, 'unique' => true, 'notnull' => false]);
+        if (!$schema->hasTable(self::UPLOAD_SESSION_TABLE)) {
+            $table = $schema->createTable(self::UPLOAD_SESSION_TABLE);
+            $table->addColumn('id', 'string', ['length' => 255, 'notnull' => false]);
+            $table->addUniqueIndex(['id']);
             $table->addColumn('parts', 'json', ['notnull' => false, 'default' => json_encode([])]);
-            $table->addColumn('totalParts', 'int', ['length' => 11, 'notnull' => false]);
-            $table->addColumn('fileSize', 'int', ['length' => 11, 'notnull' => false]);
-            $table->addColumn('assetId', 'int', ['length' => 11, 'notnull' => false]);
-            $table->addColumn('parentId', 'int', ['length' => 11, 'notnull' => false]);
-            $table->addColumn('fileName', 'varchar', ['length' => 700, 'notnull' => true]);
+            $table->addColumn('totalParts', 'integer', ['length' => 11, 'notnull' => false]);
+            $table->addColumn('fileSize', 'integer', ['length' => 11, 'notnull' => false]);
+            $table->addColumn('assetId', 'integer', ['length' => 11, 'notnull' => false]);
+            $table->addColumn('parentId', 'integer', ['length' => 11, 'notnull' => false]);
+            $table->addColumn('fileName', 'string', ['length' => 700, 'notnull' => true]);
             $table->setPrimaryKey(['id']);
+        }
+
+        $sqlStatements = $currentSchema->getMigrateToSql($schema, $db->getDatabasePlatform());
+        if (!empty($sqlStatements)) {
+            $db->executeStatement(implode(';', $sqlStatements));
         }
     }
 }
